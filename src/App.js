7a8778586ba1f14a7982a57bc62359d6b2f9356c
sup1214 <sup1214@naver.com>
2023-05-02 21:27:11 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      안녕안녕 여기가 kuniverse 홈페이지!!~~~~~리액트 만들면 서버
+      올려줄게!!키키키 다들어와
     </div>
   );
 }
